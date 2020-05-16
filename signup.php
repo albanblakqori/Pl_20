da33c675c87120a3e253997a8047acd587169779
Rexhep Krasniqi <xhkrasniqi98@gmail.com>
2020-05-16 23:30:36 +0200

    Sign up form connected with post-request

--- signup.php
+++ signup.php
@@ -3,62 +3,41 @@ require "header.php";
 ?>
 
 
-
-
-
 <div class="signup-container">
-
-<form>
+<form action="include/signup.inc.php" method="post">
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputName">Emri</label>
+      <input type="text" name="emri" class="form-control" id="inputName" placeholder="Emri juaj">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputSureame">Mbiemri</label>
+      <input type="text" name="mbiemri" class="form-control" id="inputSurname" placeholder="Mbiemri juaj">
+    </div>
+  </div>
   <div class="form-row">
     <div class="form-group col-md-6">
       <label for="inputEmail4">Adresa elektronike</label>
-      <input type="email" class="form-control" id="inputEmail4" placeholder="Adresa elektronike">
+      <input type="email" name="email" class="form-control" id="inputEmail4" placeholder="Adresa elektronike">
     </div>
     <div class="form-group col-md-6">
       <label for="inputPassword4">Fjalekalimi</label>
-      <input type="password" class="form-control" id="inputPassword4" placeholder="Fjalekalimi">
+      <input type="password" name="pwd" class="form-control" id="inputPassword4" placeholder="Fjalekalimi">
     </div>
   </div>
-  <div class="form-group">
-    <label for="inputAddress">Adresa</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Rr Agim Ramadani">
-  </div>
- 
+
   <div class="form-row">
     <div class="form-group col-md-6">
-      <label for="inputCity">Qyteti</label>
-      <input type="text" class="form-control" id="inputCity">
+      <label for="inputCity">Numri i telefonit</label>
+      <input type="tel" name="tel" class="form-control" id="inputCity" placeholder="Numri juaj i telefonit">
     </div>
-    <div class="form-group col-md-4">
-      <label for="inputState">Shteti</label>
-      <select id="inputState" class="form-control">
-        <option selected>Zgjedh...</option>
-        <option>Kosove</option>
-        <option>Shqiperi</option>
-        <option>Maqedoni</option>
-      </select>
-    </div>
-    <div class="form-group col-md-2">
-      <label for="inputZip">Kodi Postal</label>
-      <input type="text" class="form-control" id="inputZip">
+    <div class="form-group col-md-6">
+    <button style="position:fixed;right:100px;margin-top:20px;" type="submit" name="signup-button" class="btn btn-success">Regjistrohu</button>
     </div>
   </div>
-  <div class="form-group">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck">
-      <label class="form-check-label" for="gridCheck">
-        Pranoj termet dhe kushtet...
-      </label>
-    </div>
-  </div>
-<button type="submit" class="btn btn-primary">Regjistrohu</button>
+
 </form>
-
-
-
 </div>
-
-
 
 
 
